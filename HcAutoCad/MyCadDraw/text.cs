591222d5060ad791c34d76a33e50266c0ba9b8c7
李晏 <30539534@qq.com>
2020-04-08 16:51:11 +0800

    2020年4月8日16:50

---
+++ HcAutoCad/MyCadDraw/text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace MyCadDraw
+{
+    
+    public class Text
+    {
+
+        [CommandMethod("hello")]
+        public void Cadtext()
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            MessageBox.Show("hello");
+        }
+
+    }
+}
